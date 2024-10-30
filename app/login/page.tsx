d4bac5d27ce42703e5326468c1121953394b95c2
James <james@whereyouat.co.uk>
2024-10-30 20:03:54 +0000

    add force-dynamic to auth routes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 import { LoginForm } from "@/components/forms/login-form";
-
+export const dynamic = "force-dynamic";
 export default function Page() {
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,5 @@
 import { SignupForm } from "@/components/forms/signup-form";
-
+export const dynamic = "force-dynamic";
 export default function Page() {
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
